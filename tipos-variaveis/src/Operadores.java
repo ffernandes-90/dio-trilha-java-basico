a233c6e412dfdaf132ebc858e6aa3287355e327f
Felipe de Oliveira Fernandes <ffernandes.net@hotmail.com>
2023-01-14 12:02:12 -0300

    estudos com operadores lógicos

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,6 +1,7 @@
 public class Operadores {
     public static void main(String[] args){
-        /*String concatenacao ="?"; 
+        /*---------------------------CONCATENAÇÕES---------------------------:
+        String concatenacao ="?"; 
         
         concatenacao = 1+1+1+"1";
         System.out.println(concatenacao);
@@ -17,6 +18,8 @@ public class Operadores {
         concatenacao = "1"+(1+1+1);
         System.out.println(concatenacao);*/
 
+        /*---------------------------COMPARAÇÃO AVANÇADA:--------------------------- */
+        /*
         String nomeUm = "FELIPE";
         String nomeDois = new String ("FELIPE");
 
@@ -25,8 +28,8 @@ public class Operadores {
 
 
 
-
-        int numero1 = 1;
+        /*---------------------------RELACIONAIS:---------------------------*/
+        /*int numero1 = 1;
         int numero2 = 2;
 
         boolean simNao = numero1 == numero2;
@@ -43,7 +46,20 @@ public class Operadores {
 
         simNao = numero1 > numero2;
         System.out.println("numeroUm é maior que numeroDois? " + simNao);
+        --------------------------------------------------------------------*/
 
-        
+
+        /*---------------------------OPERADORES LÓGICOS--------------------------- */
+        boolean condicao1=true;
+
+        boolean condicao2=true;
+
+        if(condicao1 && (7 > 4 )){
+            System.out.println("As duas condições são verdadeiras");
+        }
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira");
+        }
+        System.out.println("Fim");
     }
 }
